5e2d1b80327faf72e7e4d4ed07dc0735546d3563
Samiiii1990 <sami.hermana@gmail.com>
2023-11-05 15:13:11 -0300

    fix port and ui

--- GestorPresupuesto/microServicioGastos/Controllers/GastosController.cs
+++ GestorPresupuesto/microServicioGastos/Controllers/GastosController.cs
@@ -11,22 +11,20 @@ namespace microServicioGastos.Controllers
     {
         // GET: api/<GastosController>
         [HttpGet]
-       public async Task<List<Gastos>> Get()
+    public async Task<List<Gastos>> Get()
+    {
+
+        List<Gastos> gastos = new List<Gastos>
         {
-                List<Gastos> gastos = new List<Gastos>();
-            {
-                gastos.Add(new Gastos()
-                {
-                    Id = 1,
-                    Fecha = DateTime.Now,
-                    Monto = 1000,
-                    Descripcion = "Gasto 1"
-                });
-            };
+            new Gastos { Id = 1, Fecha = DateTime.Now, Tipo = "Compra", Monto = 1000, Descripcion = "Gasto 1" },
+            new Gastos { Id = 2, Fecha = DateTime.Now, Tipo = "Compra", Monto = 3000, Descripcion = "Gasto 2" },
+            new Gastos { Id = 3, Fecha = DateTime.Now, Tipo = "Compra", Monto = 5000, Descripcion = "Gasto 3" },
+            new Gastos { Id = 4, Fecha = DateTime.Now, Tipo = "Compra", Monto = 7000, Descripcion = "Gasto 4" },
+            new Gastos { Id = 5, Fecha = DateTime.Now, Tipo = "Compra", Monto = 9000, Descripcion = "Gasto 5" }
+        };
 
-            return gastos;
-        }
-
+        return gastos;
+    }
         // POST api/<GastosController>
         [HttpPost]
         public void Post(Gastos value)
