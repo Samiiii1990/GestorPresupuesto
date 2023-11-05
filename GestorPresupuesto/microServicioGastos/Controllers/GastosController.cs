335dce604008bcf60c464e02e31bf319a154c215
Samiiii1990 <sami.hermana@gmail.com>
2023-11-05 12:19:07 -0300

    first commit

---
+++ GestorPresupuesto/microServicioGastos/Controllers/GastosController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace microServicioGastos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GastosController : ControllerBase
+    {
+        // GET: api/<GastosController>
+        [HttpGet]
+       public async Task<List<Gastos>> Get()
+        {
+                List<Gastos> gastos = new List<Gastos>();
+            {
+                gastos.Add(new Gastos()
+                {
+                    Id = 1,
+                    Fecha = DateTime.Now,
+                    Monto = 1000,
+                    Descripcion = "Gasto 1"
+                });
+            };
+
+            return gastos;
+        }
+
+        // POST api/<GastosController>
+        [HttpPost]
+        public void Post(Gastos value)
+        {
+            Console.WriteLine("Post");
+            Console.WriteLine(value);
+        }
+
+        // PUT api/<GastosController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, Gastos value)
+        {
+            Console.WriteLine("Put ID : " + id);
+            Console.WriteLine(value);
+        }
+
+        // DELETE api/<GastosController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            Console.WriteLine("Del ID : " + id);
+        }
+    }
+}
---
+++ GestorPresupuesto/microServicioGastos/Model/Gastos.cs
@@ -0,0 +1,31 @@
+﻿namespace Model
+{
+    public class Gastos
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Tipo { get; set; }
+        
+        public string Descripcion { get; set; }
+        public int Monto { get; set;}
+
+
+        public Gastos(int id, DateTime fecha, string tipo, string descripcion, int monto)
+        {
+            Id = id;
+            Fecha = fecha;
+            Tipo = tipo;
+            Descripcion = descripcion;
+            Monto = monto;
+        }
+
+        public Gastos()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} {Fecha} {Tipo} {Descripcion} {Monto}";
+        }
+    }
+}
