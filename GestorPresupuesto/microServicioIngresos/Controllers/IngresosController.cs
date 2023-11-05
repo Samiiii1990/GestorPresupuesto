07c3ec79f24503d3ae38828defaa455d523cf06b
Samiiii1990 <sami.hermana@gmail.com>
2023-11-05 15:40:04 -0300

    add microservicioingresos

---
+++ GestorPresupuesto/microServicioIngresos/Controllers/IngresosController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace microServicioIngresos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IngresosController : ControllerBase
+    {
+        // GET: api/<IngresosController>
+        [HttpGet]
+    public async Task<List<Ingresos>> Get()
+    {
+
+        List<Ingresos> ingresos = new List<Ingresos>
+        {
+            new Ingresos { Id = 1, Fecha = DateTime.Now, Tipo = "Venta", Monto = -1000, Descripcion = "Venta 1" },
+            new Ingresos { Id = 2, Fecha = DateTime.Now, Tipo = "Venta", Monto = -1000, Descripcion = "Venta 2" },
+            new Ingresos { Id = 3, Fecha = DateTime.Now, Tipo = "Venta", Monto = -1000, Descripcion = "Venta 3" },
+            new Ingresos { Id = 4, Fecha = DateTime.Now, Tipo = "Venta", Monto = -1000, Descripcion = "Venta 4" },
+            new Ingresos { Id = 5, Fecha = DateTime.Now, Tipo = "Venta", Monto = -1000, Descripcion = "Venta 5" }
+        };
+
+        return ingresos;
+    }
+        // POST api/<IngresosController>
+        [HttpPost]
+        public void Post(Ingresos value)
+        {
+            Console.WriteLine("Post");
+            Console.WriteLine(value);
+        }
+
+        // PUT api/<IngresosController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, Ingresos value)
+        {
+            Console.WriteLine("Put ID : " + id);
+            Console.WriteLine(value);
+        }
+
+        // DELETE api/<IngresosController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            Console.WriteLine("Del ID : " + id);
+        }
+    }
+}
---
+++ GestorPresupuesto/microServicioIngresos/Model/Ingresos.cs
@@ -0,0 +1,31 @@
+﻿namespace Model
+{
+    public class Ingresos
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Tipo { get; set; }
+        
+        public string Descripcion { get; set; }
+        public int Monto { get; set;}
+
+
+        public Ingresos(int id, DateTime fecha, string tipo, string descripcion, int monto)
+        {
+            Id = id;
+            Fecha = fecha;
+            Tipo = tipo;
+            Descripcion = descripcion;
+            Monto = monto;
+        }
+
+        public Ingresos()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} {Fecha} {Tipo} {Descripcion} {Monto}";
+        }
+    }
+}
