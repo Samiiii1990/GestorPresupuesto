018686678187fdcc015e93e4ebb3f992e090419d
Samiiii1990 <sami.hermana@gmail.com>
2023-11-09 00:04:54 -0300

    add real date

--- GestorPresupuesto/microServicioGastos/Controllers/GastosController.cs
+++ GestorPresupuesto/microServicioGastos/Controllers/GastosController.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Model;
 
-
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace microServicioGastos.Controllers
 {
     [Route("api/[controller]")]
@@ -12,20 +10,48 @@ namespace microServicioGastos.Controllers
     {
         // GET: api/<ModeloController>
         [HttpGet]
-    public async Task<List<Modelo>> Get()
-    {
-
-        List<Modelo> gastos = new List<Modelo>
+        public async Task<List<Modelo>> Get()
         {
-            new Modelo { Id = 1, Fecha = DateTime.Now, Tipo = "Compra", Monto = 1000, Descripcion = "Gasto 1" },
-            new Modelo { Id = 2, Fecha = DateTime.Now, Tipo = "Compra", Monto = 3000, Descripcion = "Gasto 2" },
-            new Modelo { Id = 3, Fecha = DateTime.Now, Tipo = "Compra", Monto = 5000, Descripcion = "Gasto 3" },
-            new Modelo { Id = 4, Fecha = DateTime.Now, Tipo = "Compra", Monto = 7000, Descripcion = "Gasto 4" },
-            new Modelo { Id = 5, Fecha = DateTime.Now, Tipo = "Compra", Monto = 9000, Descripcion = "Gasto 5" }
-        };
+            List<Modelo> gastos =
+                new List<Modelo> {
+                    new Modelo {
+                        Id = 1,
+                        Fecha = new DateTime(2023, 11, 11),
+                        Tipo = "Compra",
+                        Monto = 1000,
+                        Descripcion = "Gasto 1"
+                    },
+                    new Modelo {
+                        Id = 2,
+                        Fecha = new DateTime(2023, 11, 19),
+                        Tipo = "Compra",
+                        Monto = 3000,
+                        Descripcion = "Gasto 2"
+                    },
+                    new Modelo {
+                        Id = 3,
+                        Fecha = new DateTime(2023, 11, 12),
+                        Tipo = "Compra",
+                        Monto = 5000,
+                        Descripcion = "Gasto 3"
+                    },
+                    new Modelo {
+                        Id = 4,
+                        Fecha = new DateTime(2023, 11, 5),
+                        Tipo = "Compra",
+                        Monto = 7000,
+                        Descripcion = "Gasto 4"
+                    },
+                    new Modelo {
+                        Id = 5,
+                        Fecha = new DateTime(2023, 11, 30),
+                        Tipo = "Compra",
+                        Monto = 9000,
+                        Descripcion = "Gasto 5"
+                    }
+                };
 
-        return gastos;
-    }
-       
+            return gastos;
+        }
     }
 }
--- GestorPresupuesto/microServicioIngresos/Controllers/IngresosController.cs
+++ GestorPresupuesto/microServicioIngresos/Controllers/IngresosController.cs
@@ -2,7 +2,6 @@
 using Model;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace microServicioIngresos.Controllers
 {
     [Route("api/[controller]")]
@@ -11,19 +10,48 @@ namespace microServicioIngresos.Controllers
     {
         // GET: api/<ModeloController>
         [HttpGet]
-    public async Task<List<Modelo>> Get()
-    {
-
-        List<Modelo> ingresos = new List<Modelo>
+        public async Task<List<Modelo>> Get()
         {
-            new Modelo { Id = 1, Fecha = DateTime.Now, Tipo = "Venta", Monto = -1000, Descripcion = "Venta 1" },
-            new Modelo { Id = 2, Fecha = DateTime.Now, Tipo = "Venta", Monto = -1000, Descripcion = "Venta 2" },
-            new Modelo { Id = 3, Fecha = DateTime.Now, Tipo = "Venta", Monto = -1000, Descripcion = "Venta 3" },
-            new Modelo { Id = 4, Fecha = DateTime.Now, Tipo = "Venta", Monto = -1000, Descripcion = "Venta 4" },
-            new Modelo { Id = 5, Fecha = DateTime.Now, Tipo = "Venta", Monto = -1000, Descripcion = "Venta 5" }
-        };
+            List<Modelo> ingresos =
+                new List<Modelo> {
+                    new Modelo {
+                        Id = 1,
+                        Fecha = new DateTime(2023, 11, 2),
+                        Tipo = "Venta",
+                        Monto = 1000,
+                        Descripcion = "Venta 1"
+                    },
+                    new Modelo {
+                        Id = 2,
+                        Fecha = new DateTime(2023, 11, 4),
+                        Tipo = "Venta",
+                        Monto = 1000,
+                        Descripcion = "Venta 2"
+                    },
+                    new Modelo {
+                        Id = 3,
+                        Fecha = new DateTime(2023, 11, 7),
+                        Tipo = "Venta",
+                        Monto = 1000,
+                        Descripcion = "Venta 3"
+                    },
+                    new Modelo {
+                        Id = 4,
+                        Fecha = new DateTime(2023, 11, 9),
+                        Tipo = "Venta",
+                        Monto = 1000,
+                        Descripcion = "Venta 4"
+                    },
+                    new Modelo {
+                        Id = 5,
+                        Fecha = new DateTime(2023, 11, 22),
+                        Tipo = "Venta",
+                        Monto = 000,
+                        Descripcion = "Venta 5"
+                    }
+                };
 
-        return ingresos;
+            return ingresos;
+        }
     }
 }
-}   
